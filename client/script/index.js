f90cb5b0339e2abf041ecc8ca302da79c4e817a5
Ken Duigraha utra <kenduigraha@yahoo.com>
2016-11-21 14:32:40 +0700

    d3 hierarchy

--- client/script/index.js
+++ client/script/index.js
@@ -1,13 +1,12 @@
 'use strict'
 
-const treemap = d3.treemap()
 
-// console.log(treemap);
+
 
 $.ajax({
   url: 'http://localhost:3000/api/maps',
   success : function(data){
-    // console.log(data);
+    // bar chart
     var test = [10, 45, 30, 45, 70, 90, 100]
 
     d3.select('#bar-chart')
@@ -21,11 +20,62 @@ $.ajax({
       .style('margin-right', '5')
       .style('background-color', "lightblue")
 
-    d3.select('#mind-map')
-      .selectAll('div')
-      .data(data.children)
-      .enter()
-      .append('p')
-      .text((d) => (d.name))
+    // mind map
+    var svg = d3.select("#mind-map svg"),
+    width = +svg.attr("width"),
+    height = +svg.attr("height"),
+    g = svg.append("g").attr("transform", "translate(60,0)");
+
+    var tree = d3.cluster()
+        .size([height, width - 270]);
+
+    var stratify = d3.stratify()
+        .parentId(function(d) { return d.id.substring(0, d.id.lastIndexOf(".")); });
+
+    // d3.json(data, function(error, treeData) {
+      // if (error) throw error;
+
+      var root = d3.hierarchy(data);
+      tree(root);
+
+      // connect
+      var link = g.selectAll(".link")
+          .data(root.descendants().slice(1))
+        .enter().append("path")
+          .attr("class", "link")
+          .attr("d", function(d) {
+            return "M" + d.y + "," + d.x
+                + "C" + (d.parent.y + 100) + "," + d.x
+                + " " + (d.parent.y + 100) + "," + d.parent.x
+                + " " + d.parent.y + "," + d.parent.x;
+          });
+      // node
+      var node = g.selectAll(".node")
+          .data(root.descendants())
+        .enter().append("g")
+          .attr("class", function(d) { return "node" + (d.children ? " node--internal" : " node--leaf"); })
+          .attr("transform", function(d) {
+            return "translate(" + d.y + "," + d.x + ")";
+          })
+
+      node.append("circle")
+          .attr("r", 2.5);
+
+      node.append("text")
+          .attr("dy", 3)
+          .attr("x", function(d) { return d.children ? -8 : 8; })
+          .style("text-anchor", function(d) { return d.children ? "end" : "start"; })
+          .text(function(d) {
+            return d.data.name;
+          });
+    // });
+
+
+    // d3.select('#mind-map')
+    //   .selectAll('div')
+    //   .data(data.children)
+    //   .enter()
+    //   .append('p')
+    //   .text((d) => (d.name))
   }
 })
