12befc7a2bb1e4d61993d36d7abc01b780646bc2
Ken Duigraha utra <kenduigraha@yahoo.com>
2016-11-21 11:10:53 +0700

    rename end point to /api/maps

--- server/app.js
+++ server/app.js
@@ -23,7 +23,7 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(cors())
 
-app.use('/api/fixtures', routes);
+app.use('/api/maps', routes);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
