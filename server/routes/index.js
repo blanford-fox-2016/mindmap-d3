b89d1e4162af20ac20cc422cf13a984f61555ec0
Dharmadi Tanamas <dharmadi93@gmail.com>
2016-11-21 11:04:53 +0700

    create routes for get all maps

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,9 +1,8 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
-});
+const mapController = require('../controllers/controllers.api.maps')
+
+router.get('/', mapController.getAllMap)
 
 module.exports = router;
