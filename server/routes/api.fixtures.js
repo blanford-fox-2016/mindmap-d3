bd1c4fdaed7085ba793869320a2f9777b0789cef
Septian A Tama <sfirst.tama@gmail.com>
2016-11-21 16:21:11 +0700

    express generator without view engine

--- server/routes/api.fixtures.js
+++
@@ -1,7 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const controller = require('../controllers/api.fixtures')
-
-router.get('/', controller.getData);
-
-module.exports = router;
