fec7af18a8cf7f26d260688d6588ceeb7d1c07bd
tevinstein <tevinsa@yahoo.com>
2016-11-21 15:38:35 +0700

    cors

--- server.js
+++ server.js
@@ -2,10 +2,12 @@ var express        = require('express'),
 	app            = express(),
 	port           = process.env.PORT || 8080,
 	bodyParser     = require('body-parser'),
-	expressLayouts = require('express-ejs-layouts')
+	expressLayouts = require('express-ejs-layouts'),
+	cors           = require('cors')
 
 app.use(bodyParser.urlencoded({ extended: true }))
 
+app.use(cors())
 app.set('view engine', 'ejs')
 app.use(expressLayouts)
 app.use(express.static(__dirname + '/public'))
