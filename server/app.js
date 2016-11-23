783f72867738b4d1c23f919137b6f2a2bc3f6304
Septian A Tama <sfirst.tama@gmail.com>
2016-11-23 17:28:35 +0700

    graph ok, need style

--- server/app.js
+++ server/app.js
@@ -11,6 +11,8 @@ const apiMaps = require('./routes/apiMaps');
 
 const app = express();
 
+const cors = require('cors');
+app.use(cors());
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
